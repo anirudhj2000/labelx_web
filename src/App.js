3fa4ebf93aa6c6a1a858ed17edd026580a83aa2a
anirudhj2000 <anirudhjoshi985@gmail.com>
2023-08-08 23:22:50 +0530

    added minor file fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
 import './App.css';
 import Home from './Home';
 
 function App() {
+
+  useEffect(() => {
+    window.onload = () => {
+      document.getElementById('myVideo').play()
+    }
+  })
   return (
-    <Home/>
+    <div>
+      <video autoplay muted loop id="myVideo" >
+        <source src={"space_vid.mp4"} type="video/mp4"></source>
+      </video>
+      <div className='content' >
+      <Home/>
+      </div>
+    </div>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React ,{useEffect}from "react";
+import './home.css'
 
 const Home = () => {
+
+    
+
     return(
-        <div>
-            git
+        <div style={{color:'#fff'}}>
+            <video autoplay muted loop id="myVideo">
+                <source src="space_vid.mp4" type="video/mp4"/>
+            </video>
+            <section className="screen-section">
+                <div className="page-1">
+                    <h1>Labelx</h1>
+                    <p>LabelX is an advanced image labeling platform catering to diverse industries. With precision and efficiency, it offers tailored solutions for image training data needs. From object recognition to semantic segmentation, LabelX streamlines the process, enabling businesses to create accurate AI models while ensuring the highest standards of data quality and annotation.</p>
+                </div>
+            </section>
+            <section className="screen-section">
+                <div className="page-1">
+                    <h1>Labelx</h1>
+                    <p>LabelX is an advanced image labeling platform catering to diverse industries. With precision and efficiency, it offers tailored solutions for image training data needs. From object recognition to semantic segmentation, LabelX streamlines the process, enabling businesses to create accurate AI models while ensuring the highest standards of data quality and annotation.</p>
+                </div>
+            </section>
+            <section className="screen-section">
+                 <div className="page-1">
+                    <h1>Labelx</h1>
+                    <p>LabelX is an advanced image labeling platform catering to diverse industries. With precision and efficiency, it offers tailored solutions for image training data needs. From object recognition to semantic segmentation, LabelX streamlines the process, enabling businesses to create accurate AI models while ensuring the highest standards of data quality and annotation.</p>
+                </div>
+            </section>
         </div>
     )
 }
